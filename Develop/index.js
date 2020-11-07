30f29eae87245982cf697b1bcab247cd87e22d33
emelanson <elijahmelanson@gmail.com>
2020-11-06 22:11:58 -0500

    wrote questions and tested inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -5,8 +5,49 @@ const fs = require("fs");
 const questions = [
     {
         type: "input",
+        message: "What is your project's title?",
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "Please enter a description for your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please write any specific installation instructions here.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Please write usage guidelines here.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Please write guidelines for contribution here.",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Please write test instructions here.",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "What type of license are you distributing this project under?",
+        name: "license",
+        choices: ["CC BY", "GNU GPLv3", "MIT", "Other/I'll include my own"]
+    },
+    {
+        type: "input",
         message: "What is your Github username?",
         name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
     },
 
 ];
